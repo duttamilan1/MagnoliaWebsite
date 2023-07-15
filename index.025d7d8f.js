841a55076a2b8ca7ef73bbe7ba09d77443ab820c
duttamilan1 <67763093+duttamilan1@users.noreply.github.com>
2023-07-15 02:58:57 +0000

    Deploying to gh-pages from @ duttamilan1/MagnoliaWebsite@a57fd57a11353b3023781319c8debeee87249779 🚀

---
+++ index.025d7d8f.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".burger"),t=document.querySelector("#"+e.dataset.target);e.addEventListener("click",function(){e.classList.toggle("is-active"),t.classList.toggle("is-active")})}();
+//# sourceMappingURL=index.025d7d8f.js.map
